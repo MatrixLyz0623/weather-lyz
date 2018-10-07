441579c666aac854b52bd46dc72ffcb1b9f04bec
YongzhenLu <18500878538@163.com>
2018-10-07 20:18:19 +0800

    first commit

---
+++ app/src/main/java/com/example/a18500/weather/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.a18500.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
